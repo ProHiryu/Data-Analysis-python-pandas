fb9acaca286bcd3c9cf84bebb3a87e4b0e7092c6
ProHiryu <songheqi1996@gmail.com>
2017-04-18 21:37:22 +0800

    pickling

--- code/pickling.py
+++ code/pickling.py
@@ -5,6 +5,8 @@ import pickle
 # Not necessary, I just do this so I do not show my API key.
 api_key = open('quandlapikey.txt','r').read()
 
+api_key = api_key.strip()
+
 def state_list():
     fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
     return fiddy_states[0][0][1:]
@@ -22,7 +24,7 @@ def grab_initial_state_data():
         if main_df.empty:
             main_df = df
         else:
-            main_df = main_df.join(df)
+            main_df = main_df.merge(df)
 
     pickle_out = open('fiddy_states.pickle','wb')
     pickle.dump(main_df, pickle_out)
